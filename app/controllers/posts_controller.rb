4a32b289a88cc77b06842f93faa01b6173e49483
FreshGeek001 <memory2026@126.com>
2017-01-09 23:39:38 +0800

    extra credits

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,29 @@ class PostsController < ApplicationController
     end
     end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice:"Updated Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path, notice:"Deleted Success"
+  end
+
+
     private
 
     def post_params
